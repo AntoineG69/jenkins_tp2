c207e0e6bd2f32588d22316f549aa60132cdaeb6
Antoine <ant1.gillet@gmail.com>
2024-04-22 10:34:25 +0200

    fucj

--- server.js
+++ server.js
@@ -9,4 +9,5 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
     console.log("Lest's go ! ")
+    console.log("Lest's go ! ")
 });
