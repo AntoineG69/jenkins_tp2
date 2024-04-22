000f5413dd4a24bc17dc40c52506ad3c61f3bc76
Antoine <ant1.gillet@gmail.com>
2024-04-22 10:03:50 +0200

    second commit'

--- server.js
+++ server.js
@@ -8,4 +8,5 @@ app.get('/', (req, res) => {
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
+    console.log("Lest's go ! ")
 });
